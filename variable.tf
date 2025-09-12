3a999e1164e38f60bf053b684bcfd38c38c19076
sunil-th <sunilcr200@gmail.com>
2025-09-12 22:20:02 +0530

    Update variable.tf -1

--- variable.tf
+++ variable.tf
@@ -1,10 +1,24 @@
+# Default values
 variable "filename" {
-default ="pets.txt"
   description = "The name of the file to create"
-  type        = string  
-  }
+  type        = string
+  default     = "pets.txt"
+}
+
 variable "content" {
-default ="I love tigers"
   description = "The content to put inside the file"
+  type        = string
+  default     = "I love tigers"
+}
+
+# No default (forces interactive prompt)
+variable "prefix" {
+  description = "Prefix for file naming (example: MR, DR, SR)"
+  type        = string
+}
+
+# No default (can be set via CLI flags or env var)
+variable "owner" {
+  description = "Owner of the file"
   type        = string
 }
