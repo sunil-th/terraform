90eecb01e2c69953ab92b94c38a50c1465ceca10
sunil-th <sunilcr200@gmail.com>
2025-09-12 22:41:15 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,23 +2,11 @@
 variable "filename" {
   description = "The name of the file to create"
   type        = string
-  default     = "pets.txt"
+  default     = "Devops.txt"
 }
 
 variable "content" {
   description = "The content to put inside the file"
   type        = string
-  default     = "I love tigers"
-}
-
-# No default (forces interactive prompt)
-variable "prefix" {
-  description = "Prefix for file naming (example: MR, DR, SR)"
-  type        = string
-}
-
-# No default (can be set via CLI flags or env var)
-variable "owner" {
-  description = "Owner of the file"
-  type        = string
+  default     = "Hello guys welcome to  Devops Cloud"
 }
